6b1de1c8b80e9524f0fb0f04cc50445102b4479e
thetrueyoshifan41 <thetrueyoshifan41@gmail.com>
2020-04-23 04:03:07 -0500

    Added a wrapper for VRChat's input box, and began work on SocialMenuFunctions

---
+++ emmVRC/Hacks/SocialMenuFunctions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+using VRC.Social;
+using VRC;
+using VRC.Core;
+using VRC.UI;
+
+namespace emmVRC
+{
+    internal class socialMenuFunctions
+    {
+        private static GameObject SocialFunctionsButton = new GameObject();
+        private static GameObject UserSendMessage = new GameObject();
+        private static GameObject UserNotes = new GameObject();
+        private static GameObject TeleportButton = new GameObject();
+        private static GameObject WorldNotesButton = new GameObject();
+        private static GameObject ToggleBlockButton = new GameObject();
+
+        internal static void Start()
+        {
+            SocialFunctionsButton = GameObject.Instantiate(GameObject.Find("MenuContent/Screens/UserInfo/User Panel/Playlists/PlaylistsButton"), GameObject.Find("MenuContent/Screens/UserInfo/User Panel/Playlists").transform);
+
+        }
+    }
+}
---
+++ emmVRC/Libraries/InputUtilities.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace emmVRC.Libraries
+{
+    public class InputUtilities
+    {
+        public static void OpenInputBox(string title, string buttonText, Action<string> acceptAction) 
+        {
+            VRCUiPopupManager.field_VRCUiPopupManager_0.Method_Public_String_String_InputType_Boolean_String_Action_3_String_List_1_KeyCode_Text_Action_String_Boolean_Action_1_VRCUiPopup_0(title, "", UnityEngine.UI.InputField.InputType.Standard, false, buttonText, UnhollowerRuntimeLib.DelegateSupport.ConvertDelegate<Il2CppSystem.Action<string, Il2CppSystem.Collections.Generic.List<UnityEngine.KeyCode>, UnityEngine.UI.Text>>((System.Action<string, Il2CppSystem.Collections.Generic.List<UnityEngine.KeyCode>, UnityEngine.UI.Text>)((string s, Il2CppSystem.Collections.Generic.List<UnityEngine.KeyCode> k, UnityEngine.UI.Text t) => { acceptAction.Invoke(s); })), null);
+            /*VRCUiPopupManager.field_VRCUiPopupManager_0.Method_Public_String_String_InputType_String_Action_3_String_List_1_KeyCode_Text_Action_String_Boolean_Action_1_VRCUiPopup_0(
+                title, 
+                "", 
+                UnityEngine.UI.InputField.InputType.Standard, 
+                buttonText, 
+                UnhollowerRuntimeLib.DelegateSupport.ConvertDelegate<Il2CppSystem.Action<string, Il2CppSystem.Collections.Generic.List<UnityEngine.KeyCode>, UnityEngine.UI.Text>>((System.Action<string, Il2CppSystem.Collections.Generic.List<UnityEngine.KeyCode>, UnityEngine.UI.Text>)((string s, Il2CppSystem.Collections.Generic.List<UnityEngine.KeyCode> k, UnityEngine.UI.Text t) => {
+                acceptAction.Invoke(s);
+            })), null);*/
+        }
+    }
+}
