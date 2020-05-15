1eef01bfc75e71e306a07ca2543474b91858a2b0
Brandon Thompson <XhailOrigin@gmail.com>
2020-05-15 15:21:16 -0700

    added callback for destroy and init networkclient

--- emmVRC/Network/NetworkClient.cs
+++ emmVRC/Network/NetworkClient.cs
@@ -21,26 +21,34 @@ namespace emmVRC.Network
         //TODO add sockets
         private static string BaseAddress = "http://crreamhub.com";
         private static int Port = 3000;
-        public static string baseURL{ get { return BaseAddress + ":" + Port; } }
+        public static string baseURL { get { return BaseAddress + ":" + Port; } }
         private static string _authToken;
-        public static string authToken { 
-            get { return _authToken; } 
+        public static string authToken {
+            get { return _authToken; }
             set
             {
                 _authToken = value;
                 httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", _authToken);
-            } 
+            }
         }
 
         public static HttpClient httpClient { get; set; }
 
-        public static void InitializeClient()
+        public static T InitializeClient<T>(Func<T> callback = null)
         {
             httpClient = new HttpClient();
             httpClient.DefaultRequestHeaders.Accept.Clear();
             httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
             httpClient.DefaultRequestHeaders.UserAgent.ParseAdd("emmVRC/1.0 (Client; emmVRCClient/"+ Attributes.Version+")");
             login();
+            return callback();
+        }
+
+        public static T DestroyClient<T>(Func<T> callback = null)
+        {
+            httpClient = null;
+            authToken = null;
+            return callback();
         }
 
         public static void login()
