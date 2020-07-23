fcbe731b4c8ffcf0e473f2f926c5ca7e10d2cd0e
thetrueyoshifan41 <thetrueyoshifan41@gmail.com>
2020-07-23 08:52:22 -0500

    Rolled back Master Crown to v1.5.0's version

--- emmVRC/Objects/Attributes.cs
+++ emmVRC/Objects/Attributes.cs
@@ -8,9 +8,9 @@ namespace emmVRC.Objects
 {
     public class Attributes
     {
-        public static string Version = "1.6.0";
+        public static string Version = "1.6.1";
         public static bool Beta = false;
-        public static string DateUpdated = "7/20/2020";
+        public static string DateUpdated = "7/23/2020";
         //  "This is a reference for how long the text can be in the Changelog"
         //  "before it rolls off the page."
         public static string Changelog = 
@@ -21,7 +21,7 @@ namespace emmVRC.Objects
             "• Added lots of optimizations in order to enhance FPS while \n" +
             "using various functions of emmVRC\n";
         public static bool Debug = false;
-        public static string TargetMelonLoaderVersion = "0.2.4";
+        public static string TargetMelonLoaderVersion = "0.2.5";
         public static string TargetemmVRCLoaderVersion = "1.0.0";
         public static string[] IncompatibleMelonLoaderVersions = { "0.1.0", "0.2.0", "0.2.1", "0.2.2", "0.2.3" };
         public static string[] IncompatibleemmVRCLoaderVersions = { "0.0.1", "0.0.2", "0.0.3", "0.0.4", "0.0.5" };
